1e7f66200f910e6efd4ca0e43b7e10a92be8a8ff
Sirbi <osilvio123@gmail.com>
2025-03-14 19:07:52 -0300

    feat: adição do link

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import "./App.css"
+ 
+
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RotinaPage from "./pages/rotina/page";
 import TemplatePage from "./pages/template/page";
+import ModalBlank from "./pages/blank_modal/modal"
 
 function App() {
   return (
@@ -10,8 +13,11 @@ function App() {
       <Routes>
         <Route path="/rotina" element={<RotinaPage />} />
         <Route path="/template" element={<TemplatePage />} />
+        <Route path="/blank_modal" element={<ModalBlank />}>
+        </Route>
       </Routes>
     </Router>
+    
   );
 }
 
