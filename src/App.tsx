712cefece6583f6c9491306fb20080522e767521
EduSG <eduardo.gomes.072020@gmail.com>
2025-03-14 11:03:33 -0300

    fix: ajustes na navegação

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,18 @@
-import './App.css'
-import {MagnifyingGlass, PlusCircle } from "@phosphor-icons/react";
+import "./App.css"
 
-function rotinaForm() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RotinaPage from "./pages/rotina/page";
+import TemplatePage from "./pages/template/page";
 
+function App() {
   return (
-    
-  <div className="font-[Poppins]  p-4 bg-gray-100 min-h-screen">
-  <h2 className="text-xl font-semibold mb-4">Minhas Rotinas</h2>
-
-    <div className='flex items-end gap-6 border border-gray-300 p-6 w-full'>
-
-    <div className="flex flex-col">
-      <label className='text-gray-500 text-sm'>Nome Rotina</label>
-      <input type="text" className='border border-gray-300 p-1 rounded-md'></input>
-    </div>
-
-    <div className="flex flex-col">
-      <label className='text-gray-500 text-sm'>Nome Template</label>
-      <input className='border border-gray-300 p-1 rounded-md'></input>
-    </div>
-
-    <div className="flex flex-col">
-      <label className='text-gray-500 text-sm'>Tipo Template</label>
-      <input className='border border-gray-300 p-1 rounded-md'></input>
-    </div>
-
-    <div className="flex flex-col">
-      <label className='text-gray-500 text-sm'>Status</label>
-      <select className='w-full border border-gray-300 p-1 rounded-md text-gray-500' > 
-        <option  value="#" disabled hidden>Selecione</option>
-        <option  value="Ativo">Ativo</option>
-        <option  value="Inativo">Inativo</option>
-      </select>
-    </div>
-      
-
-
- <button className="bg-orange-500 text-white px-4 py-2 rounded-sm flex items-center gap-2 cursor-pointer"><MagnifyingGlass size={20} weight="bold" /> Pesquisar</button>
-    <button className="bg-cyan-500 text-white px-4 py-2 rounded-sm flex items-center gap-2 cursor-pointer"><PlusCircle size={20} weight="bold"/>Novo Template</button>
-    </div>
-  </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/rotina" element={<RotinaPage />} />
+        <Route path="/template" element={<TemplatePage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default rotinaForm
+export default App;
--- src/pages/rotina/page.tsx
+++ src/pages/rotina/page.tsx
@@ -1,24 +1,38 @@
-import './App.css'
 import {MagnifyingGlass, PlusCircle } from "@phosphor-icons/react";
 
-function templateForm() {
+export default function RotinaPage() {
 
   return (
     
   <div className="font-[Poppins]  p-4 bg-gray-100 min-h-screen">
-  <h2 className="text-xl font-semibold mb-4">Meus Templates</h2>
+  <h2 className="text-xl font-semibold mb-4">Minhas Rotinas</h2>
 
     <div className='flex items-end gap-6 border border-gray-300 p-6 w-full'>
 
     <div className="flex flex-col">
-      <label className='text-gray-500 text-sm'>Nome Template</label>
+      <label className='text-gray-500 text-sm'>Nome Rotina</label>
       <input type="text" className='border border-gray-300 p-1 rounded-md'></input>
+    </div>
+
+    <div className="flex flex-col">
+      <label className='text-gray-500 text-sm'>Nome Template</label>
+      <input className='border border-gray-300 p-1 rounded-md'></input>
     </div>
 
     <div className="flex flex-col">
       <label className='text-gray-500 text-sm'>Tipo Template</label>
       <input className='border border-gray-300 p-1 rounded-md'></input>
     </div>
+
+    <div className="flex flex-col">
+      <label className='text-gray-500 text-sm'>Status</label>
+      <select className='w-full border border-gray-300 p-1 rounded-md text-gray-500' > 
+        <option  value="#" disabled hidden>Selecione</option>
+        <option  value="Ativo">Ativo</option>
+        <option  value="Inativo">Inativo</option>
+      </select>
+    </div>
+      
 
 
  <button className="bg-orange-500 text-white px-4 py-2 rounded-sm flex items-center gap-2 cursor-pointer"><MagnifyingGlass size={20} weight="bold" /> Pesquisar</button>
@@ -28,4 +42,3 @@ function templateForm() {
   )
 }
 
-export default templateForm
