c902a16a218d07d4a355a14504f79ef9d3af2b8d
Anderson <anderfilhopaulo@gmail.com>
2025-03-14 01:39:49 -0300

    templateForm e rotinaForm base criados

---
+++ e-mail-api-frontend/src/screens/template/templateForm.tsx
@@ -0,0 +1,31 @@
+import './App.css'
+import {MagnifyingGlass, PlusCircle } from "@phosphor-icons/react";
+
+function templateForm() {
+
+  return (
+    
+  <div className="font-[Poppins]  p-4 bg-gray-100 min-h-screen">
+  <h2 className="text-xl font-semibold mb-4">Meus Templates</h2>
+
+    <div className='flex items-end gap-6 border border-gray-300 p-6 w-full'>
+
+    <div className="flex flex-col">
+      <label className='text-gray-500 text-sm'>Nome Template</label>
+      <input type="text" className='border border-gray-300 p-1 rounded-md'></input>
+    </div>
+
+    <div className="flex flex-col">
+      <label className='text-gray-500 text-sm'>Tipo Template</label>
+      <input className='border border-gray-300 p-1 rounded-md'></input>
+    </div>
+
+
+ <button className="bg-orange-500 text-white px-4 py-2 rounded-sm flex items-center gap-2 cursor-pointer"><MagnifyingGlass size={20} weight="bold" /> Pesquisar</button>
+    <button className="bg-cyan-500 text-white px-4 py-2 rounded-sm flex items-center gap-2 cursor-pointer"><PlusCircle size={20} weight="bold"/>Novo Template</button>
+    </div>
+  </div>
+  )
+}
+
+export default templateForm
