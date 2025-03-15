b4d81f73c7e97e136dbd0d1a34bd1f68559358ec
Anderson <anderfilhopaulo@gmail.com>
2025-03-15 01:09:36 -0300

    fix/template-base e rotina-base responsivos

--- src/pages/template/page.tsx
+++ src/pages/template/page.tsx
@@ -1,27 +1,27 @@
-import {MagnifyingGlass, PlusCircle } from "@phosphor-icons/react";
+import {MagnifyingGlass, Plus } from "@phosphor-icons/react";
 
 export default function TemplatePage() {
 
   return (
     
-  <div className="font-[Poppins]  p-4 bg-gray-100 min-h-screen">
-  <h2 className="text-xl font-semibold mb-4">Meus Templates</h2>
+  <div className="bg-[#EDF1F5] min-h-screen">
+  <h2 className="text-xl py-6 font-semibold pl-4">Meus Templates</h2>
 
-    <div className='flex items-end gap-6 border border-gray-300 p-6 w-full'>
+    <div className='flex flex-wrap md:flex-nowrap items-end gap-4 border border-[#D1D5DB] p-6 w-full'>
 
     <div className="flex flex-col">
-      <label className='text-gray-500 text-sm'>Nome Template</label>
-      <input type="text" className='border border-gray-300 p-1 rounded-md'></input>
+      <label className='text-[#929292] text-sm'>Nome Template</label>
+      <input type="text" className='border border-[#D9D9D9] p-1.25 rounded-md focus:outline-none focus:ring-1 focus:ring-[#a8a3a3]'></input>
     </div>
 
     <div className="flex flex-col">
-      <label className='text-gray-500 text-sm'>Tipo Template</label>
-      <input className='border border-gray-300 p-1 rounded-md'></input>
+      <label className='text-[#929292] text-sm'>Tipo Template</label>
+      <input className='border border-[#D9D9D9] p-1.25 rounded-md focus:outline-none focus:ring-1 focus:ring-[#a8a3a3]'></input>
     </div>
 
 
- <button className="bg-orange-500 text-white px-4 py-2 rounded-sm flex items-center gap-2 cursor-pointer"><MagnifyingGlass size={20} weight="bold" /> Pesquisar</button>
-    <button className="bg-cyan-500 text-white px-4 py-2 rounded-sm flex items-center gap-2 cursor-pointer"><PlusCircle size={20} weight="bold"/>Novo Template</button>
+ <button className="bg-[#ED6F2A] text-[#FFFFFF] px-4 py-1.5 rounded-sm flex items-center gap-2 cursor-pointer hover:bg-[#ed6e2aee] active:bg-[#BA6F47] transition delay-60 duration-40 ease-in-out"><MagnifyingGlass size={20} weight="bold" /> Pesquisar</button>
+    <button className="bg-[#46B7BA] text-[#FFFFFF] px-4 py-1.5 rounded-sm flex items-center gap-0.5 cursor-pointer hover:bg-[#46b6baf3] active:bg-[#1096DE] transition delay-60 duration-40 ease-in-out"><Plus size={20} weight="bold"/>Novo Template</button>
     </div>
   </div>
   )
